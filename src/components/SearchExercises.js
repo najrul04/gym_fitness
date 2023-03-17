b2a064e57302f5abd538f1dd5f1e983dd24d43d2
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-03-17 15:41:11 +0600

    adding useEffect

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,8 @@
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchExercises = () => {
+    const [search, setSearch] = useState('second')
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
@@ -11,8 +12,8 @@ const SearchExercises = () => {
         <TextField
           height="76px"
           sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
-        //   value={search}
-        //   onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
