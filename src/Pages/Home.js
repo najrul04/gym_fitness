30e8f46a6c553fc87dffd31426383c2ad2299dae
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-03-17 13:31:48 +0600

    creating excercises, hero banner and serach excercises

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Box } from '@mui/material';
+import React, { useState } from 'react';
+
+import Exercises from '../components/Exercises';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
 
-export default Home
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
+  );
+};
+
+export default Home;
