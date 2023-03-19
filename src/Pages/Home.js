591a315719366ccfc4a22f3012bc57b9cc0f6083
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-03-19 19:21:37 +0600

    finished exercise

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,6 +8,7 @@ import SearchExercises from '../components/SearchExercises';
 const Home = () => {
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState('all');
+  console.log(bodyPart)
 
   return (
     <Box>
