76f459f1a7e776615053e91e0a405fab9d10b866
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-03-19 19:55:05 +0600

    Youtube Search and Download api subscribed

--- src/Pages/ExerciseDetail.js
+++ src/Pages/ExerciseDetail.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const {id} = useParams();
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+
+    }
+    fetchExercisesData();
+  }, [id])
+  
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+        <Detail exerciseDetail={exerciseDetail}/>
+        <ExerciseVideos/>
+        <SimilarExercises/>
+    </Box>
   )
 }
 
